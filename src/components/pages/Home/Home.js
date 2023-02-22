84f02fa3fc8c3695299cd5d45604dad528743301
dalimkumar604 <dalimkumar212582@gmail.com>
2023-02-23 01:04:55 +0600

    third

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import FomoMagazine from './FomoMagazine/FomoMagazine';
-import Hero from './Hero/Hero';
-import News from './News/News';
-import Subscribe from './Subscribe/Subscribe';
-import Trust from './Trust/Trust';
+import React from "react";
+import FomoMagazine from "./FomoMagazine/FomoMagazine";
+import Hero from "./Hero/Hero";
+import News from "./News/News";
+import Subscribe from "./Subscribe/Subscribe";
+import Trust from "./Trust/Trust";
 
 const Home = () => {
-    return (
-        <div>
-            <Hero />
-            <Trust />
-            <News />
-            <Subscribe />
-        </div>
-    );
+  return (
+    <div>
+      <Hero />
+      <Trust />
+      {/* <News /> */}
+      <Subscribe />
+    </div>
+  );
 };
 
 export default Home;
